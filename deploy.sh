811bc761d4522f516b9b93ebb2677a7b6f3608fb
Afoninsky <vkfont@gmail.com>
2016-09-16 20:03:24 +0300

    deployment

--- deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Exit on any error
-set -e
-
-sudo /opt/google-cloud-sdk/bin/gcloud docker push us.gcr.io/${PROJECT_NAME}/hello
-sudo chown -R ubuntu:ubuntu /home/ubuntu/.kube
-kubectl patch deployment micro-test -p '{"spec":{"template":{"spec":{"containers":[{"name":"micro-test","image":"us.gcr.io/spair-api/hello:'"$CIRCLE_SHA1"'"}]}}}}'
